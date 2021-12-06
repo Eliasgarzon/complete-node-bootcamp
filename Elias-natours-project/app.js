1447cb34a26493d3a25a7e177c8d6b1f3bf11dcd
Elias Garzon <eliasgarzon@gmail.com>
2021-12-06 18:14:12 +0200

    up to lesson 68

---
+++ Elias-natours-project/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+//MIDDLEWHERE
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+// this is how to serve static files
+// app.use(express.static(`${__dirname}/public`));
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
