c3569cffe0197a0d9bb747c15f4614c4b31f1133
Elias Garzon <eliasgarzon@gmail.com>
2021-12-08 12:25:03 +0200

    until L100: created APIFeatures class

--- Elias-natours-project/controllers/tourController.js
+++ Elias-natours-project/controllers/tourController.js
@@ -1,9 +1,67 @@
+const ApiFeatures = require("./../utils/ApiFeatures");
 const fs = require("fs");
 const Tour = require("./../models/tourModel");
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price";
+
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
+  //note we need to exclude special field names
   try {
-    const tours = await Tour.find();
+    // //1 FILTERING
+    // const queryObj = { ...req.query };
+    // const excludedFields = ["page", "sort", "limit", "fields"];
+    // excludedFields.forEach((el) => delete queryObj[el]);
+
+    // //the special queries like gt or lt come built on the req.query BUT we need to add the mongo operator $
+
+    // // 2 ADVANCE FILTER
+    // let queryStr = JSON.stringify(queryObj);
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // let query = Tour.find(JSON.parse(queryStr));
+
+    // //3 SORTING
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(",").join(" ");
+    //   query = query.sort(sortBy);
+    // } else {
+    //   query = query.sort("-createdAt");
+    // }
+
+    //4 FIELD LIMITING
+    // if (req.query.fields) {
+    //   let fields = req.query.fields.split(",").join(" ");
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select("-__v");
+    // }
+
+    //5 PAGINATION
+
+    // const page = +req.query.page || 1;
+    // const limit = +req.query.limit || 100;
+    // const skip = (page - 1) * limit;
+    // query = query.skip(skip).limit(limit);
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments();
+    //   if (skip >= numTours) throw new Error("This page does not exist");
+    // }
+
+    //EXECUTE QUERY
+    const features = new ApiFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const tours = await features.query;
+
+    //SEND RESPONSE
     res.status(200).json({
       status: "success",
       requestTime: req.requestTime,
@@ -12,9 +70,9 @@ exports.getAllTours = async (req, res) => {
       },
     });
   } catch (err) {
-    res.status(400).json({
+    res.status(404).json({
       status: "fail",
-      message: "cant get all tours",
+      message: err,
     });
   }
 };
