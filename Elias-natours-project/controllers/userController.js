4a6d3744516ac61e2b3b47cd27156aa350af3663
Elias Garzon <eliasgarzon@gmail.com>
2021-12-11 17:24:01 +0200

    until L 140

--- Elias-natours-project/controllers/userController.js
+++ Elias-natours-project/controllers/userController.js
@@ -3,6 +3,16 @@ const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("../utils/AppError");
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const user = await User.find();
 
@@ -12,6 +22,43 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: {
       user,
     },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) create error if user posts password
+  if (req.body.email) {
+    return next(new AppError("This route is not for email updates."));
+  }
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use /updateMyPassword"
+      )
+    );
+  }
+
+  //filtered unwanted field names for security
+  const filteredBody = filterObj(req.body, "name", "email");
+
+  //update the user
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    user: updatedUser,
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: "success",
+    data: null,
   });
 });
 
