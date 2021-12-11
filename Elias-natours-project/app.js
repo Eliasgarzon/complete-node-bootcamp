0ba3c5bc48e9e22076055c956602512c76ef59b9
Elias Garzon <eliasgarzon@gmail.com>
2021-12-11 18:20:33 +0200

    until L 145

--- Elias-natours-project/app.js
+++ Elias-natours-project/app.js
@@ -1,20 +1,66 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const AppError = require("./utils/AppError");
 const globalErrorHandler = require("./controllers/errorController");
 
-//MIDDLEWHERE
+//GLOBAL MIDDLEWHERE
+//set security HTTP headers
+app.use(helmet());
+
+//developmnet login
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use(express.json());
+
+//body parser
+app.use(express.json({ limit: "10kb" }));
+
+//data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//data sanitization against XSS
+app.use(xss());
+
+//prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
+//serving static files
+app.use(express.static(`${__dirname}/public`));
+
+//test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
+//Limit request from same IP
+const limiter = rateLimit({
+  max: 200,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
 // this is how to serve static files
 // app.use(express.static(`${__dirname}/public`));
 
